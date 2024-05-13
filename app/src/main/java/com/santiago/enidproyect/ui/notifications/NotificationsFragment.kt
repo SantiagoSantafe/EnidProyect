823f913110aceb45319695dfc9df8ce406c03290
Santiago Santafe <andressasi@unisabana.edu.co>
2024-05-12 22:31:53 -0500

    Perfil

--- app/src/main/java/com/santiago/enidproyect/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/santiago/enidproyect/ui/notifications/NotificationsFragment.kt
@@ -7,11 +7,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
 import com.santiago.enidproyect.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
+    lateinit var auth: FirebaseAuth
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,10 +29,14 @@ class NotificationsFragment : Fragment() {
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
 
-        val textView: TextView = binding.textNotifications
+        val textUserName: TextView = binding.textUserName
+        val textUserEmail: TextView = binding.textUserEmail
         notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            textUserName.text = user?.displayName.toString()
+            textUserEmail.text = user?.email.toString()
         }
         return root
     }
