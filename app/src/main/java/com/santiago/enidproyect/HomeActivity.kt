7780b893e363ae2f7e7039726e38a515e827cd9b
Santiago Santafe <andressasi@unisabana.edu.co>
2024-05-22 19:00:08 -0500

    Busqueda funciona

--- app/src/main/java/com/santiago/enidproyect/HomeActivity.kt
+++ app/src/main/java/com/santiago/enidproyect/HomeActivity.kt
@@ -1,21 +1,53 @@
 package com.santiago.enidproyect
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.santiago.enidproyect.databinding.ActivityHomeBinding
+import com.santiago.enidproyect.ui.chat.DashboardFragment
+import com.santiago.enidproyect.ui.chat.User
+import com.santiago.enidproyect.ui.chat.userAdapter
+import com.santiago.enidproyect.ui.notifications.NotificationsFragment
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var search_btn: ImageView
+    private lateinit var search_bar: EditText
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: userAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+        userList = ArrayList()
+
+        adapter = userAdapter(this, userList)
+
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -34,5 +66,42 @@ class HomeActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        if (mAuth.currentUser == null) {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+            finish()
+        }
+        search_btn = findViewById(R.id.search_btn)
+        search_bar = findViewById(R.id.search_bar)
+        search_bar.visibility = View.GONE
+        search_btn.visibility = View.GONE
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.navigation_dashboard) {
+                search_btn.visibility = View.VISIBLE
+            } else {
+                search_btn.visibility = View.GONE
+            }
+        }
+
+        //Aqui tengo que añadir la logica para
+        mDbRef.child("user").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userList.clear()
+                for (postsnapshot in snapshot.children) {
+                    val currentUser = postsnapshot.getValue(User::class.java)
+                    if (mAuth.currentUser?.uid != currentUser?.uid) {
+                        userList.add(currentUser!!)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+
+
     }
+
 }
--- app/src/main/java/com/santiago/enidproyect/ui/chat/DashboardFragment.kt
+++ app/src/main/java/com/santiago/enidproyect/ui/chat/DashboardFragment.kt
@@ -1,10 +1,14 @@
 package com.santiago.enidproyect.ui.chat
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -16,6 +20,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.santiago.enidproyect.R
 import com.santiago.enidproyect.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -26,6 +31,9 @@ class DashboardFragment : Fragment() {
     private lateinit var adapter: userAdapter
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mDbRef: DatabaseReference
+    private lateinit var search_btn: ImageView
+    private lateinit var search_bar: EditText
+
 
     private val binding get() = _binding!!
 
@@ -41,6 +49,27 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+
+        search_btn = requireActivity().findViewById(R.id.search_btn)
+        search_bar = requireActivity().findViewById(R.id.search_bar)
+
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Este método se llama antes de que se realice un cambio de texto.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Este método se llama mientras se realiza un cambio de texto.
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Este método se llama después de que se haya realizado un cambio de texto.
+                val nuevoTexto = s.toString()
+                filterUsers(nuevoTexto)
+            }
+        }
+
         mAuth = FirebaseAuth.getInstance()
         mDbRef = FirebaseDatabase.getInstance().getReference()
         userList = ArrayList()
@@ -49,6 +78,15 @@ class DashboardFragment : Fragment() {
         userRecyclerView = _binding!!.userRecycler
         userRecyclerView.layoutManager = LinearLayoutManager(context)
         userRecyclerView.adapter = adapter
+        search_btn.setOnClickListener {
+            if (search_bar.visibility == View.VISIBLE) {
+                search_bar.visibility = View.GONE
+                search_bar.setText("")
+            } else {
+                search_bar.visibility = View.VISIBLE
+                search_bar.addTextChangedListener(textWatcher)
+            }
+        }
         //Aqui tengo que añadir la logica para
         mDbRef.child("user").addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -70,5 +108,25 @@ class DashboardFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    fun filterUsers(texto: String) {
+        mDbRef.child("user").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userList.clear()
+                for (postsnapshot in snapshot.children) {
+                    val currentUser = postsnapshot.getValue(User::class.java)
+                    if (mAuth.currentUser?.uid != currentUser?.uid) {
+                        // Add user to list if not the current user
+                        userList.add(currentUser!!)
+                    }
+                }
+                val filteredList = userList.toList()
+                    .filter { usuario -> usuario.name?.contains(texto, ignoreCase = true) ?: false }
+                adapter.update(filteredList)
+            }
+            override fun onCancelled(error: DatabaseError) {
+                // Handle possible errors
+            }
+        })
     }
 }
