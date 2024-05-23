128d3b213d613b50ae012b3ad70ef0ce2ef2d59a
Santiago Santafe <andressasi@unisabana.edu.co>
2024-05-22 21:28:21 -0500

    Version casi final

--- app/src/main/java/com/santiago/enidproyect/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/santiago/enidproyect/ui/chat/ChatActivity.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -18,6 +19,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 import com.santiago.enidproyect.R
 import java.text.SimpleDateFormat
 
@@ -31,7 +33,8 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var mDbRef: DatabaseReference
     private lateinit var btnDevolverse : ImageView
     private lateinit var userText : TextView
-
+    private lateinit var userImage : ImageView
+private lateinit var  usuarioRecibe : User
 
     var receiveRoom: String? = null
     var senderRoom: String? = null
@@ -58,9 +61,28 @@ class ChatActivity : AppCompatActivity() {
         chatRecyclerView.adapter = messageAdapter
         btnDevolverse = findViewById(R.id.backButton)
         userText = findViewById(R.id.userName)
+        userImage = findViewById(R.id.userImage)
         userText.text = name
+        usuarioRecibe = User()
+        if (receiverUid != null) {
+            mDbRef.child("user").child(receiverUid).get().addOnSuccessListener {
+                val user = it.getValue(User::class.java)
+                if (user != null) {
+                    // Mostrar la información del usuario
+                    usuarioRecibe = user
+                    val storageReference = FirebaseStorage.getInstance().getReference("images/${user.email}.jpg")
+                    storageReference.downloadUrl.addOnSuccessListener { uri ->
+                        Glide.with(this).load(uri).into(userImage)
+                    }.addOnFailureListener {
+                        // Manejar cualquier error
+                    }
+                }
+            }
+        }
+
+
         userText.setOnClickListener {
-            showUserInfoDialog()
+            showUserInfoDialog(usuarioRecibe)
         }
         btnDevolverse.setOnClickListener {
             finish()
@@ -97,12 +119,10 @@ class ChatActivity : AppCompatActivity() {
             chatRecyclerView.scrollToPosition(messageList.size - 1)
         }
     }
-    private fun showUserInfoDialog() {
-        // Simula la obtención de información del usuario
-        // Debes reemplazar esto con tu lógica para obtener datos reales, por ejemplo, desde Firebase
+    fun showUserInfoDialog(user: User) {
         val userInfo = """
-        Nombre: ${userText.text}
-        Email: ${FirebaseAuth.getInstance().currentUser?.email}
+        Nombre: ${user.name}
+        Email: ${user.email}
     """.trimIndent()
 
         // Crear el constructor del diálogo
